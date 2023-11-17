ab5ee0dccaf3f4b4d6202ab9e499eb4ae3ae8cd5
takhan <tauseefak93@gmail.com>
2023-11-17 20:56:20 +0000

    more changes

--- pages/1_Plotting_Demo.py
+++ pages/1_Plotting_Demo.py
@@ -12,12 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, WebRtcMode
 import streamlit as st
 import time
 import queue
 import pydub
 import speech_recognition as sr
+from aiortc.contrib.media import MediaRecorder
+import uuid
+from pathlib import Path
+from utils import get_ice_servers
+
+RECORD_DIR = Path("./records")
+RECORD_DIR.mkdir(exist_ok=True)
 
 webrtc_ctx = webrtc_streamer(key="sample", media_stream_constraints={"video": False, "audio": True})
 status_indicator = st.empty()
@@ -26,23 +33,57 @@ text_output = st.empty()
 stream = None
 recognizer = sr.Recognizer()
 
-while True:
-    if webrtc_ctx.audio_receiver:
-        sound_chunk = pydub.AudioSegment.empty()
-        try:
-            audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
-        except queue.Empty:
-            time.sleep(0.1)
-            status_indicator.write("No frame arrived.")
-            continue
+if "prefix" not in st.session_state:
+    st.session_state["prefix"] = str(uuid.uuid4())
+    prefix = st.session_state["prefix"]
+    in_file = RECORD_DIR / f"{prefix}_input.mp4"
+    out_file = RECORD_DIR / f"{prefix}_output.mp4"
 
-        status_indicator.write("Running. Say something!")
+    def in_recorder_factory() -> MediaRecorder:
+        return MediaRecorder(
+            str(in_file), format="mp4"
+        )  # HLS does not work. See https://github.com/aiortc/aiortc/issues/331
 
-        for audio_frame in audio_frames:
-            sound = pydub.AudioSegment(
-                data=audio_frame.to_ndarray().tobytes(),
-                sample_width=audio_frame.format.bytes,
-                frame_rate=audio_frame.sample_rate,
-                channels=len(audio_frame.layout.channels),
+    def out_recorder_factory() -> MediaRecorder:
+        return MediaRecorder(str(out_file), format="mp4")
+
+    webrtc_streamer(
+        key="record",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration={"iceServers": get_ice_servers()},
+        media_stream_constraints={
+            "video": False,
+            "audio": True,
+        },
+        in_recorder_factory=in_recorder_factory,
+    )
+    if in_file.exists():
+        with in_file.open("rb") as f:
+            st.download_button(
+                "Download the recorded video without video filter", f, "input.flv"
             )
-            sound_chunk += sound
+    if out_file.exists():
+        with out_file.open("rb") as f:
+            st.download_button(
+                "Download the recorded video with video filter", f, "output.flv"
+            )
+#while True:
+    #if webrtc_ctx.audio_receiver:
+        #sound_chunk = pydub.AudioSegment.empty()
+        #try:
+            #audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
+        #except queue.Empty:
+            #time.sleep(0.1)
+            #status_indicator.write("No frame arrived.")
+            #continue
+
+        #status_indicator.write("Running. Say something!")
+
+        #for audio_frame in audio_frames:
+            #sound = pydub.AudioSegment(
+                #data=audio_frame.to_ndarray().tobytes(),
+                #sample_width=audio_frame.format.bytes,
+                #frame_rate=audio_frame.sample_rate,
+                #channels=len(audio_frame.layout.channels),
+            #)
+            #sound_chunk += sound
--- utils.py
+++ utils.py
@@ -16,6 +16,14 @@ import inspect
 import textwrap
 
 import streamlit as st
+import logging
+import os
+
+import streamlit as st
+from twilio.base.exceptions import TwilioRestException
+from twilio.rest import Client
+
+logger = logging.getLogger(__name__)
 
 
 def show_code(demo):
@@ -26,3 +34,35 @@ def show_code(demo):
         st.markdown("## Code")
         sourcelines, _ = inspect.getsourcelines(demo)
         st.code(textwrap.dedent("".join(sourcelines[1:])))
+
+def get_ice_servers():
+    """Use Twilio's TURN server because Streamlit Community Cloud has changed
+    its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
+    We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
+    but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656  # noqa: E501
+    See https://github.com/whitphx/streamlit-webrtc/issues/1213
+    """
+
+    # Ref: https://www.twilio.com/docs/stun-turn/api
+    try:
+        account_sid = st.secrets["TWILIO_ACCOUNT_SID"]
+        auth_token = st.secrets["TWILIO_AUTH_TOKEN"]
+    except KeyError:
+        logger.warning(
+            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
+        )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    client = Client(account_sid, auth_token)
+
+    try:
+        print("Turn server attempted")
+        token = client.tokens.create()
+    except TwilioRestException as e:
+        st.warning(
+            f"Error occurred while accessing Twilio API. Fallback to a free STUN server from Google. ({e})"  # noqa: E501
+        )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    return token.ice_servers
+
